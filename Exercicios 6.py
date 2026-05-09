2a48c0718cff10bb785c956bdfeba084b9f68e21
mazimcosta <monarquiarox@gmail.com>
2026-05-09 16:59:34 -0300

    primeira versao do exercicio 6

--- Exercicios 6.py
+++ Exercicios 6.py
@@ -26,6 +26,8 @@ RESTRIÇÕES:
 # =========================
 # TODOS OS TESTES JUNTOS
 # =========================
+import math
+
 
 testes = [
     ("arquivo", "linha 1\nlinha 2\nlinha 3"),
@@ -41,3 +43,59 @@ testes = [
     ("dicionario", {"valor": 150}),
     ("dicionario", {"preco": 100}),
 ]
+
+
+def importar_dado(fonte:str,dado:any):
+    variavel_auxiliar=''
+    qtd_linhas=0
+    resultado={}
+        
+    if fonte.lower()=='arquivo':
+        try:
+            dado=str(dado)
+        except TypeError:
+            return resultado.update({'fonte':fonte,'dado':dado,'sucess':False,'motivo':'Valor invalido'})
+
+        for letra in dado:
+            if letra in ["\'",'n']:
+                variavel_auxiliar+=letra
+                if variavel_auxiliar=="\n":
+                    qtd_linhas+=1
+                    variavel_auxiliar=''
+        resultado.update({'fonte':fonte,'dado':dado,'sucess':True,'quantidade de linhas':qtd_linhas})
+
+        if fonte.lower()=='numero':
+            try:
+                dado=float(dado)
+                raiz=math.sqrt(dado)
+            except ValueError:
+                return resultado.update({'fonte':fonte,'dado':dado,'sucess':False,'motivo':'Valor invalido.'})
+            resultado.update({'fonte':fonte,'dado':dado,'sucess':True,'resultado':raiz})
+
+        if fonte.lower()=='lista':
+            try:
+                indice=dado[0]
+            except IndexError:
+                resultado.update({'fonte':fonte,'dado':dado,'sucess':False,'motivo':'Valor invalido.'})
+            resultado.update({'fonte':fonte,'dado':dado,'sucess':True,'resutado':indice})
+
+        if fonte.lower()=='dicionario':
+            try:
+                for chave,valor in dado:
+                    key=chave
+                    value=valor
+            except IndexError:
+                resultado.update({'fonte':fonte,'dado':dado,'sucess':False,'motivo':'valor invalido.'})
+            resultado.update({'fonte':fonte,'dado':dado,'sucess':True,'resultado':f'{key=},{value=}'})
+        
+        return resultado
+    
+
+for fonte,dado in testes:
+    resultado_final=importar_dado(fonte,dado)
+
+print(resultado_final)
+
+
+        
+
