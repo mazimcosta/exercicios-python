1dac3c8f520cb8507884eed12d449768a6d7edf6
mazimcosta <monarquiarox@gmail.com>
2026-05-11 21:31:34 -0300

    versão 3 do exercicio 8

--- Exercicios 8.py
+++ Exercicios 8.py
@@ -36,46 +36,66 @@ RESTRIÇÕES:
 
 #SOLUÇÃO:
 def adicionar_item(cardapio:dict,codigo:str,nome:str,preco:float,disponivel:bool):
-    try:
-        codigo=str(codigo)
-        nome=str(nome)
-        preco=float(preco)
-        disponivel=bool(disponivel)
-    except (ValueError,TypeError):
-        return "Erro: Dados invalidos"
+    if nome is not isinstance(nome,str):
+        return f'Erro: nome precisa ser um texto'
+    if preco<0:
+        return f'Erro: preco não pode ser negativo'
+    elif preco is not isinstance(preco,(int,float)):
+        return f' preco invalido'
+    if disponivel not in [True,False]:
+        return f'Erro:disponibilidade errada'
+    
     cardapio[codigo]={
         'nome':nome,
         'preco':preco,
-        'disponivel':disponivel
+        'disponibilidade':disponivel
+
     }
     return cardapio
 
 def atualizar_preco(cardapio:dict,codigo:str,novo_preco:float):
     if novo_preco<0:
-        return f'Erro: preco não pode ser negativo'
-    
+        return False
     try:
-
-      for chave in cardapio.keys():
-          if chave==codigo:
-              cardapio[chave]['preco']=novo_preco
+        cardapio[codigo]['preco']=float(novo_preco)
+        return True
     except KeyError:
         return False
-    return True
+
+def itens_disponiveis(cardapio:dict):
+    lista=[]
+    for item in cardapio.values():
+        if item['disponivel']==True:
+            lista.append(item['nome'])
+        if not lista:
+            return f'Não ha itens disponiveis'
+    return lista
 
 def preco_medio(cardapio:dict):
-    total=0
-    for codigo in cardapio:
-        if cardapio[codigo]['disponivel']==True:
-             total+=cardapio[codigo]['preco']
-    media=total/len(cardapio.keys())
-    media=round(media,2)
-    return media
-
-
+    lista=[]
+    media=0
+    for item in cardapio.items():
+        if item['disponivel']==True:
+            lista.append(item['preco'])
+    try:
+        media=sum(lista)/len(lista)
+        media=round(media,2)
+        return media
+    except ZeroDivisionError:
+        return f' Não ha itens disponiveis'
     
 def buscar_por_faixa(cardapio:dict,minimo:float,maximo:float):
-    lista=[cardapio['codigo']['nome'],cardapio['codigo']['preco'] for cardapio in cardapio if cardapio['codigo']['preco']>=minimo
-           and cardapio['codigo']['preco']<=maximo]
+    lista=[{'nome':item['nome'],'preco':item['preco']} for item in cardapio.items() if minimo<=item['preco']<=maximo]
     return lista
+
+itens=[('456','feijoada',45.8,True),
+       ('4512','macarronada',25.2,False),
+       ('8795','pastelao',52.3,True)]
+
+for codigo,nome,preco,disponivel in itens:
+    cardapio=adicionar_item(codigo=codigo,nome=nome,preco=preco,disponivel=disponivel)
+
+print(cardapio)
+
+    
 
