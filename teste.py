b758b7f68c2fc8b38eb6af7533e234f1870c1118
mazimcosta <monarquiarox@gmail.com>
2026-05-13 07:20:19 -0300

    retirando excesso de arquivos

--- teste.py
+++
@@ -1,9 +0,0 @@
-'''testando'''
-
-dicionario={'arroz':{'quantidade':10,'preco':5.0},
-            'feijao':{'quantidade':15,'preco':478}}
-
-for produto,(quantidade, preco)  in dicionario.items():
-    if quantidade==10:
-        print(f'{produto=},{quantidade=}')
-    
