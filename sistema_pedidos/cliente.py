48ff4df282dc39172772d2c307a5455175747061
mazimcosta <monarquiarox@gmail.com>
2026-05-16 17:35:16 -0300

    criando a classe cliente e produto

--- sistema_pedidos/cliente.py
+++ sistema_pedidos/cliente.py
@@ -3,11 +3,11 @@
 class Cliente:
 
     def __init__(self,nome:str,email:str):
-        if not nome.isalpha():
+        if not nome.replace(' ','').isalpha():
             raise ValueError(' Nome precisa ser um texto')
-        elif '@' not in email:
+        elif '@' not in email and '.' not in email.split('@')[1]:
             raise ValueError('Email invalido')
-        elif not email.endswith('.com') or not email.endswith('.br'):
+        elif not (email.endswith('.com') or  email.endswith('.br')):
             raise ValueError('Email invalido')
         self.nome=nome
         self.email=email
--- sistema_pedidos/produto.py
+++ sistema_pedidos/produto.py
@@ -4,22 +4,30 @@
 class Produto:
 
     def __init__(self,nome:str,preco:float,estoque:int):
+        if not isinstance(preco,float):
+            raise ValueError('Preco invalido')
+        elif preco<0:
+            raise ValueError('Preco invalido')
+        elif not isinstance(nome,str):
+            raise ValueError('Nome precisa ser um texto')
+        elif  not nome.replace(' ','').isalpha():
+            raise ValueError('Nome deve conter apenas letras')
+        elif estoque<0:
+            raise ValueError('Estoque não pode ser negativo')
         self.nome=nome
         self.preco=preco
         self.estoque=estoque
 
-    def baixar_estoque(self,quantidade):
-        if self.estoque==0:
-            raise ValueError(' Não ha produtos no estoque')
-        if quantidade>self.estoque:
-            raise ValueError('Estoque insuficiente')
-        self.estoque-=quantidade
-        return f' Quantidade retirada com sucesso'
-    
-    def devolver_estoque(self,quantidade):
-        if quantidade<=0:
+    def baixar_estoque(self,valor):
+        if valor<0:
             raise ValueError('Valor invalido')
-        self.estoque+=quantidade
-        return f' Estoque reposto com sucesso'
+        elif valor>self.estoque:
+            raise ValueError('Estoque insuficiente')
+        self.estoque-=valor
+        return f'Estoque reduzido com sucesso'
     
-    
+    def devolver_estoque(self,valor):
+        if valor<0:
+            raise ValueError('Valor invalido')
+        self.estoque+=valor
+        return f'Estoque reposto com sucesso'
